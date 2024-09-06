327e1c905b9a960a476268db91836cb575718826
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-09-05 22:22:20 -0400

    Deck wip

--- cards/src/Deck.js
+++ cards/src/Deck.js
@@ -5,3 +5,15 @@ import "./Deck.css";
 
 const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
+function Deck() {
+  const [deck, setDeck] = useState(null);
+  const [drawn, setDrawn] = useState([]);
+  const [isShuffling, setIsShuffling] = useState(false);
+
+  useEffect(function loadDeckFromAPI() {
+    async function fetchData() {
+        const d = await axios.get(`${API_BASE_URL}/new/shuffle`);
+        setDeck(d.data);
+    }
+  fetchData();
+}, []);
