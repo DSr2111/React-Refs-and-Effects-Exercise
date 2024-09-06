3bc27ff16abad8b018333155edb2a167fdc51d10
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-09-05 22:18:34 -0400

    API variable and importing react

--- cards/src/Deck.js
+++ cards/src/Deck.js
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 import axios from "axios";
 import "./Deck.css";
+
+const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
+
